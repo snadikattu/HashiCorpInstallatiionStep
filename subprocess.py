1f307eada9b25a236260173aaf77353ed949ab25
snadikattu <48895176+snadikattu@users.noreply.github.com>
2023-05-12 09:57:47 -0500

    Update subprocess.py

--- subprocess.py
+++ subprocess.py
@@ -1,14 +1,37 @@
 import subprocess
+import os
+import json
 
-def run_terraform_cmd(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+class TerraformClient:
+    def __init__(self, working_dir):
+        self.working_dir = working_dir
 
-    if process.returncode != 0:
-        print(f'Error running terraform command: {stderr.decode()}')
-        return None
+    def init(self):
+        self._run_terraform_cmd(['terraform', 'init'])
 
-    return stdout.decode()
+    def apply(self, auto_approve=True):
+        cmd = ['terraform', 'apply']
+        if auto_approve:
+            cmd.append('-auto-approve')
+        self._run_terraform_cmd(cmd)
+
+    def destroy(self, auto_approve=True):
+        cmd = ['terraform', 'destroy']
+        if auto_approve:
+            cmd.append('-auto-approve')
+        self._run_terraform_cmd(cmd)
+
+    def _run_terraform_cmd(self, cmd):
+        process = subprocess.Popen(cmd, cwd=self.working_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+
+        if process.returncode != 0:
+            print(f'Error running terraform command: {stderr.decode()}')
+            return None
+
+        return stdout.decode()
 
 # Usage
-print(run_terraform_cmd(['terraform', 'apply', '-auto-approve']))
+tf = TerraformClient('/path/to/your/terraform/files')
+tf.init()
+tf.apply()
